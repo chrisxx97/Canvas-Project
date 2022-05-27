c1323bc12a4c181ce4cfd11d1512ff0170930587
yichiehc <48332564+yichiehc@users.noreply.github.com>
2022-05-26 20:35:06 -0500

    Added sqlite database

--- canvas/backend/app.py
+++ canvas/backend/app.py
@@ -1,8 +1,12 @@
 from flask import Flask
 from flask_cors import CORS
+import sqlite3
 
 app = Flask(__name__)
 cors = CORS(app)
+
+connect = sqlite3.connect('sqlite/canvas.db')
+cursor = connect.cursor()
 
 @app.route('/test')
 def my_profile():
