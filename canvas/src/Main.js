2ced4c93dcbec5061fbd4c7d7b4c48cab290ba2f
yichiehc <48332564+yichiehc@users.noreply.github.com>
2022-06-01 20:56:55 -0500

    Fixed logo

--- canvas/src/Main.js
+++ canvas/src/Main.js
@@ -27,14 +27,12 @@ class Main extends Component {
                             <ul id="side-menu">
                             <li id="logo">
                                 <NavLink id="logo-a" to="/">
-                                    <img id="logo-img" src="assets/UChicago_Shield_2Color_Maroon_WhiteBorder_RGB.png"
+                                    <img id="logo-img" src={require("./assets/UChicago_Shield_2Color_Maroon_WhiteBorder_RGB.png")}
                                         alt="logo" />
                                 </NavLink>
                             </li>
                             <li className="menu-item">
                                 <NavLink to="/account">
-                                    <img src="https://canvas.uchicago.edu/images/messages/avatar-50.png"
-                                        alt="account" />
                                     <div>Account</div>
                                 </NavLink>
                             </li>
